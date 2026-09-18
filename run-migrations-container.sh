bb58ab8a873c2357c97e91ec55db10b119d79287
Antony Mwangi <amburu415@gmail.com>
2026-09-18 12:32:27 +0300

    Delete run-migrations-container.sh

--- run-migrations-container.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-dotnet tool install --global dotnet-ef
-export PATH=$PATH:/root/.dotnet/tools
-cd /src
-dotnet ef database update --project backend/src/CommerceEdge.Infrastructure --startup-project backend/src/CommerceEdge.Api --connection 'Server=mssql,1433;Database=CommerceEdge;User Id=sa;Password=Abc.123!;TrustServerCertificate=true'
